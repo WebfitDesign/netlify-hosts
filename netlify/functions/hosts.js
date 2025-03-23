d82c4eb8ba8687e31f1eedcb87060114f5a57c93
Daniel <125220325+WebfitDesign@users.noreply.github.com>
2025-03-23 16:17:48 +0000

    Added O

--- netlify/functions/hosts.js
+++ netlify/functions/hosts.js
@@ -2,7 +2,7 @@ const handler = async () => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: "daniel".toUpperCase(),
+    body: "danielO".toUpperCase(),
   }
 }
 
